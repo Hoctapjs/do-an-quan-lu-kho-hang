ddc8c9af326b80f0f75dd69962329aff655c7abe
unknown <thanhsonhoctap1255@gmail.com>
2023-04-15 18:58:54 +0700

    khai bao thu vien + dinh nghia kieu du lieu + ham xu ly nut

--- code.c
+++ code.c
@@ -1,1 +1,34 @@
 // day la file code
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef int ItemType;
+
+struct SNode
+{
+    ItemType info;
+    SNode* next;
+
+};
+
+struct SList
+{
+    SNode* head;
+    SNode* tail;
+};
+
+SNode* creatsnode(ItemType x)
+{
+    SNode* p = new SNode;
+    if (p == NULL)
+    {
+        printf("\nkhong the cap phat nut moi!");
+        getch();
+        return NULL;
+    }
+    p->info = x;
+    p->next = NULL;
+    return p;
+}
